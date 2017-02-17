e94b557b5528cbc65c7d11941a4b3fd814c73d94
knarf7112 <knarf7112@gmail.com>
2017-02-18 01:03:37 +0800

    #0006: 修正字體位置並建立Resume prototype

---
+++ JS/myResume.js
@@ -0,0 +1,15 @@
+﻿var resume = {
+    author: "楊世嘉",
+    LastUpdateTime: "2016-01-23",
+    version: "1.0.0",
+    Init: init
+};
+
+function init(){
+
+}
+
+var app = angular.module('MyApp', []);
+app.controller('Ctrl1',['$scope',function($scope){
+    $scope.name = 'Hi I am Knock Yang';
+}]);
