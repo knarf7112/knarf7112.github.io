00bf58291e2edfab8e08f95003c2cb5e88b6f3e8
knock <knarf7112@gmail.com>
2021-02-15 22:59:54 +0800

    update infinite scroller

---
+++ Project/InfiniteScroller/data.js
@@ -0,0 +1,212 @@
+const testData = [
+    {
+        "id": 135925727,
+        "name": "30_Day_Practice_JavaScript_Webbos",
+        "description": "JavaScript 30 days - Wesbos",
+        "clone_url": "https://github.com/knarf7112/30_Day_Practice_JavaScript_Webbos.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 34848410,
+        "name": "ActiveMQLab",
+        "description": "使用ActiveMQ+BatchAP上傳資料",
+        "clone_url": "https://github.com/knarf7112/ActiveMQLab.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 27219469,
+        "name": "AOPTest",
+        "description": "AOP(Aspect Orientent Programming)練習-動態設定新增物件透過介面",
+        "clone_url": "https://github.com/knarf7112/AOPTest.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 28614128,
+        "name": "AsyncSocketTest",
+        "description": "Socket Server 測試當傳輸檔案的Server",
+        "clone_url": "https://github.com/knarf7112/AsyncSocketTest.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 35399966,
+        "name": "AttributeTest",
+        "description": "used attribute and reflection to check poco's properties length if poco has markup the attribute",
+        "clone_url": "https://github.com/knarf7112/AttributeTest.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 38021097,
+        "name": "BackgroundWorkerTest",
+        "description": "測試用的BackgroundWorker",
+        "clone_url": "https://github.com/knarf7112/BackgroundWorkerTest.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 34843744,
+        "name": "BankServer",
+        "description": "對銀行端的代理Socket",
+        "clone_url": "https://github.com/knarf7112/BankServer.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 334597653,
+        "name": "blog",
+        "description": "个人博客，分享一些前端工作学习中的收获，欢迎star",
+        "clone_url": "https://github.com/knarf7112/blog.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 27105274,
+        "name": "FrmMultiThreadUI",
+        "description": "練習使用非同步方式(照黑大的PDF練習)",
+        "clone_url": "https://github.com/knarf7112/FrmMultiThreadUI.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 27103707,
+        "name": "FrmPDF",
+        "description": "使用iTextSharp第三方套件練習建立與讀取PDF檔",
+        "clone_url": "https://github.com/knarf7112/FrmPDF.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 35400718,
+        "name": "FTPTest",
+        "description": "HW5_FTP操作",
+        "clone_url": "https://github.com/knarf7112/FTPTest.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 58070029,
+        "name": "growth-ebook",
+        "description": "Ebook of Growth Engineering: The Definitive Guide。全栈增长工程师指南",
+        "clone_url": "https://github.com/knarf7112/growth-ebook.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 303362980,
+        "name": "Hahow-Lunix-",
+        "description": "[Hahow:線上課程]Linux 我來教: CentOS/ RHEL 8 新世代雲端",
+        "clone_url": "https://github.com/knarf7112/Hahow-Lunix-.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 40235554,
+        "name": "HttpClient_Lib",
+        "description": "use System.Net.Http.HttpClient to send request and get response",
+        "clone_url": "https://github.com/knarf7112/HttpClient_Lib.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 34452351,
+        "name": "ISO8583Parser",
+        "description": "ISO8583Parser",
+        "clone_url": "https://github.com/knarf7112/ISO8583Parser.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 49336501,
+        "name": "jData",
+        "description": "for update json and get json",
+        "clone_url": "https://github.com/knarf7112/jData.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 44869736,
+        "name": "knarf7112.github.io",
+        "description": "My WebPage",
+        "clone_url": "https://github.com/knarf7112/knarf7112.github.io.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 49711136,
+        "name": "Learn-Git-in-30-days",
+        "description": "這是 Will 保哥在 2013 第 6 屆 iT 邦幫忙鐵人賽年度大獎的得獎著作【30 天精通 Git 版本控管】，歡迎大家 fork 我，如果有看見任何文字勘誤，也歡迎利用 pull request 來通知我修正，謝謝！",
+        "clone_url": "https://github.com/knarf7112/Learn-Git-in-30-days.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 35530179,
+        "name": "MyDesignPattern_Test",
+        "description": "DesignPattern practice",
+        "clone_url": "https://github.com/knarf7112/MyDesignPattern_Test.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 227965268,
+        "name": "node-express-course",
+        "description": "An introduction to Node.js and Express.js servers",
+        "clone_url": "https://github.com/knarf7112/node-express-course.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 39228966,
+        "name": "Proxy",
+        "description": "Authentication smart card",
+        "clone_url": "https://github.com/knarf7112/Proxy.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 26898022,
+        "name": "SocketClient",
+        "description": "非同步socket服務測試",
+        "clone_url": "https://github.com/knarf7112/SocketClient.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 36857910,
+        "name": "SocketClient.Domain",
+        "description": "Socket的client端的測試方案只有Socket.Domain專案寫的比較完整可用",
+        "clone_url": "https://github.com/knarf7112/SocketClient.Domain.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 41733646,
+        "name": "SocketServer",
+        "description": "Socket Server",
+        "clone_url": "https://github.com/knarf7112/SocketServer.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 27532436,
+        "name": "SpringADO",
+        "description": null,
+        "clone_url": "https://github.com/knarf7112/SpringADO.git",
+        "updated_at": "2021-02-12T07:12:44Z"
+    },
+    {
+        "id": 35469799,
+        "name": "TestProject2015-05-12",
+        "description": "test project",
+        "clone_url": "https://github.com/knarf7112/TestProject2015-05-12.git",
+        "updated_at": "2021-02-12T07:12:45Z"
+    },
+    {
+        "id": 38573520,
+        "name": "Test_3DHomeWork",
+        "description": "test three.js and practice",
+        "clone_url": "https://github.com/knarf7112/Test_3DHomeWork.git",
+        "updated_at": "2021-02-12T07:12:45Z"
+    },
+    {
+        "id": 36228562,
+        "name": "Test_DynamicLoadDLL",
+        "description": "(Practice) Load DLL used Reflection and  running the dynamic class method",
+        "clone_url": "https://github.com/knarf7112/Test_DynamicLoadDLL.git",
+        "updated_at": "2021-02-12T07:12:45Z"
+    },
+    {
+        "id": 41139302,
+        "name": "Test_FileWatcher",
+        "description": "測試_簡易檔案監控",
+        "clone_url": "https://github.com/knarf7112/Test_FileWatcher.git",
+        "updated_at": "2021-02-12T07:12:45Z"
+    },
+    {
+        "id": 37461626,
+        "name": "Test_JavaScript",
+        "description": "測試JavaScript的Web Site",
+        "clone_url": "https://github.com/knarf7112/Test_JavaScript.git",
+        "updated_at": "2021-02-12T07:12:45Z"
+    }
+];
